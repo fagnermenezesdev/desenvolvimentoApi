d5c2f6ca9fe4a683f367739c153344be61671604
fagnermenezesdev <fagnertavaresdemenezes2023@gmail.com>
2023-11-14 20:36:37 -0300

    arquivo

--- src/main/java/beneficiarios/api/HelloController.java
+++ src/main/java/beneficiarios/api/HelloController.java
@@ -1,4 +1,13 @@
 package beneficiarios.api;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class HelloController {
+
+    public String olaMundo(){
+        return "Hello World";
+    }
 }
