64d48f49857366d668e28174bd1f2b560426e2dd
fagnermenezesdev <fagnertavaresdemenezes2023@gmail.com>
2023-11-14 20:41:45 -0300

    ajuste metodo

--- src/main/java/beneficiarios/api/HelloController.java
+++ src/main/java/beneficiarios/api/HelloController.java
@@ -1,12 +1,14 @@
 package beneficiarios.api;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping("/hello")
 public class HelloController {
 
+    @GetMapping
     public String olaMundo(){
         return "Hello World";
     }
